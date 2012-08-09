04895166899f1ba0636aa25f75e4d6ac0535bda8
Jacob Hoffman <jhoff462@gmail.com>
2012-08-09 15:08:57 -0700

    Committ before adding external jar fix.
    The jars are included in the project, but do not get added at runtime.
    This means compilation is fine, but when we run the plugin the jars are
    not visible to the eclipse project. Committing my changes before I
    attempt to fix this.

--- org.uat.microchip.toolchain/src/org/uat/microchip/debug/model/DeviceDebugger.java
+++ org.uat.microchip.toolchain/src/org/uat/microchip/debug/model/DeviceDebugger.java
@@ -12,6 +12,18 @@ import org.openide.util.Lookup;
 import com.microchip.mplab.mdbcore.assemblies.AssemblyFactory;
 import com.microchip.mplab.mdbcore.assemblies.Assembly;
 
+//Tool Meta
+import com.microchip.mplab.mdbcore.platformtool.PlatformToolMeta;
+import com.microchip.mplab.mdbcore.platformtool.PlatformToolMetaManager;
+
+//tool Id
+import com.microchip.mplab.comm.MPLABCommProviderInterface;
+
+//Assembly Factory
+import org.openide.util.Lookup;
+import com.microchip.mplab.mdbcore.assemblies.AssemblyFactory;
+import com.microchip.mplab.mdbcore.assemblies.Assembly;
+
 //Tool Properties
 import java.util.Properties;
 
@@ -30,23 +42,67 @@ import java.util.Map;
 import org.omg.PortableInterceptor.SYSTEM_EXCEPTION;
 
 /**
- * Responsible for connecting to the device and programming it. 
- * A wrapper around MDBCore. 
- * @author Jacob 
- *
- */
+* Responsible for connecting to the device and programming it. 
+* A wrapper around MDBCore. 
+* @author Jacob 
+*
+*/
 public class DeviceDebugger {
 
 	//TODO: Abstract this so one this can be used for debug or programming.  
 	
-	//Responsible for creating the Assembly. 
+	/**
+	 * Responsible for creating the Assembly. 
+	 */
 	private AssemblyFactory assemblyFactory = null;
-	//The MDBCore super class that creates and contains all of the classes and information necessary to use MDBCore
+	/**
+	 * The MDBCore super class that creates and contains all of the classes and information necessary to use MDBCore
+	 */
 	private Assembly assembly = null;
-	//Main MDB core class
+	/**
+	 * Main MDB core class
+	 */
 	private Debugger MDB = null;
-	//Answers questions about the kind of tool that can load applications. 
+	/**
+	 * Answers questions about the kind of tool that can load applications. 
+	 */
 	private PlatformToolMeta meta;
+	/**
+	 * The file to load on to the device. 
+	 */
+	private String FILE_NAME;
+	/**
+	 * Should we output the results to the console? 
+	 */
+	private boolean outputResults = true;
+	/**
+	 * The device to program. 
+	 */
+	private String deviceName;
+	/**
+	 * The type of tool which will be used. 
+	 */
+	private String toolType;
+	/**
+	 * The id of the tool to use. 
+	 */
+	private String toolID;
 	
+	/**
+	 * Initalize MDB and connect to the device. 
+	 * @param connectionType
+	 */
+	public Boolean Connect(CONNECTION_TYPE connectionType) {
+					
+		return true;
+	}
+	
+	/**
+	 * Given the parameters and properties specified in the project, Initialize MDBCore. 
+	 */
+	public void InitalizeMDB(){
+		
+	}
 	
 }
+
